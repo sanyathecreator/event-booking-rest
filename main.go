5be9c0f02202c6f54381629ef12727aee4675a6f
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-14 18:38:37 +0100

    setup gin server and first route

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// setup http server
+	server := gin.Default()
+
+	// set a handler for GET request
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 }
