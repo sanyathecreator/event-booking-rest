aee14b25659ca27bd1bf296c5f6cd5890f776f54
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-15 20:01:14 +0100

    add update event route

--- models/event.go
+++ models/event.go
@@ -83,3 +83,22 @@ func GetEventById(id int64) (*Event, error) {
 
 	return &event, nil
 }
+
+func (event Event) UpdateEvent() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.ID)
+
+	return err
+}
--- routes/events.go
+++ routes/events.go
@@ -62,3 +62,38 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	_, err = models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBind(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+
+	err = updatedEvent.UpdateEvent()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated succesfully!"})
+}
