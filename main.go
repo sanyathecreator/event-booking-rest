f2ac55fd216dec376bb7e97efb9b8abde1f1f990
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-15 19:34:35 +0100

    add get event by id route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"sanyathecreator.com/eb-rest/db"
@@ -16,6 +17,7 @@ func main() {
 
 	// Register route handlers
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // listen on localhost:8080
@@ -31,6 +33,24 @@ func getEvents(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 // createEvent parses a JSON request body into an Event and returns it.
